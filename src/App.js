49dedeec99e8cd1c595c4d7425288709cc795a79
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-03-21 19:58:53 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import QueryInput from './components/QueryInput';
-import QueryOutput from './components/QueryOutput';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import QueryInput from "./components/QueryInput";
+import QueryOutput from "./components/QueryOutput";
 
 function App() {
-  const [query,setquery]=useState("");
+  const [query, setquery] = useState("");
   const [result, setresult] = useState([]);
   const [selectState, setSelectState] = useState("");
 
-  const fetchQuery =()=>{
-    fetch(`https://hn.algolia.com/api/v1/search?query=${query}&tags=${selectState}&hitsPerPage=1000`)
-    .then((response)=>
-      response.json()
-    ).then((r)=>{setresult((p)=>{
-
-      return r.hits;
-    }
-      
-      
-      
-      )})
-  }
+  const fetchQuery = () => {
+    fetch(
+      `https://hn.algolia.com/api/v1/search?query=${query}&tags=${selectState}&hitsPerPage=1000`
+    )
+      .then((response) => response.json())
+      .then((r) => {
+        setresult((p) => {
+          return r.hits;
+        });
+      });
+  };
   useEffect(() => {
     fetch(`http://hn.algolia.com/api/v1/search?query=`)
-    .then((response)=>
-      response.json()
-    ).then((r)=>{setresult((p)=>{
-
-      return r.hits;
-    }
-      
-      
-      
-      )})
-   
-  }, [])
+      .then((response) => response.json())
+      .then((r) => {
+        setresult((p) => {
+          return r.hits;
+        });
+      });
+  }, []);
   return (
     <div className="App">
-      <QueryInput selectState={selectState} setSelectState={setSelectState} query={query} 
-    setquery={setquery}
-    fetchQuery={fetchQuery}/>
-    <QueryOutput result={result} query={query} setresult={setresult}/>
+      <QueryInput
+        selectState={selectState}
+        setSelectState={setSelectState}
+        query={query}
+        setquery={setquery}
+        fetchQuery={fetchQuery}
+      />
+      <QueryOutput result={result} query={query} setresult={setresult} />
     </div>
   );
 }
